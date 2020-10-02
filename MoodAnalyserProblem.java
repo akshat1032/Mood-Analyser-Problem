e470a6bfe67f157cf71e486d83757d33d5d1d8f2
akshat1032 <akshat.30oct@gmail.com>
2020-10-02 13:34:34 +0530

    Added : Welcome message added

---
+++ MoodAnalyserProblem.java
@@ -0,0 +1,10 @@
+package com.capgemini.moodanalyserproblem;
+
+public class MoodAnalyserProblem {
+	
+	public static void main(String[] args) {
+		
+		//Welcome message added
+		System.out.println("Welcome to Mood Analyser Problem");
+	}
+}
